3c5425c924413b66808bd607e0a6c3c9d514240e
zawataku <zactaku_0819@yahoo.co.jp>
2023-05-24 10:35:02 +0900

    月が正しく表示されないバグの修正

--- js/clock.js
+++ js/clock.js
@@ -9,7 +9,7 @@ function clock() {
     var time = new Date();
 
     var year = (time.getFullYear());
-    var month = (time.getMonth());
+    var month = (time.getMonth() + 1);
     var date = (time.getDate());
 
     var hour = setzero(time.getHours());
