e18b38e50951fcde3baf3300985b9bb6ee9d37cb
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-05-18 00:19:58 +0900

    Productsのレイアウト著性

--- src/components/products/product_card.tsx
+++ src/components/products/product_card.tsx
@@ -1,38 +1,27 @@
 import React from "react";
-import Link from "next/link";
 
 type ProductProps = {
     name: string
     description: string
-    language: string
     imgURL: string
     linkURL: string
 }
 
-export default function ProductCard({ name, description, language, imgURL, linkURL }: ProductProps) {
+export default function ProductCard({ name, description, imgURL, linkURL }: ProductProps) {
     return (
-        <li>
-            <ul className="mt-3 flex gap-x-3">
-                <Link href={linkURL}>
-                    <div className="m-2 space-y-2">
-                        <div className="group flex flex-col gap-1 rounded-lg p-5 text-gray">
-                            <div className="shadow-xl group relative m-0 flex h-72 w-72 rounded-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
-                                <div className="z-10 h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
 
-                                    <img src={imgURL} className="animate-fade-in block h-full w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110" alt="" />
-                                </div>
+        <a className="group" href={linkURL}>
+            <div className="relative overflow-hidden rounded-xl pt-[50%] sm:pt-[70%]">
+                <img className="absolute start-0 top-0 size-full rounded-xl object-cover transition-transform duration-500 ease-in-out group-hover:scale-110" src={imgURL} alt="Image Description" />
+            </div>
 
-                                <div className="text-center justify-center content-center bg-slate-500 p-3 rounded-xl opacity-90 absolute bottom-0 z-20 m-0 w-full h-full transition duration-300 ease-in-out group-hover:opacity-0">
-                                    <h1 className="text-3xl font-bold text-slate-50">{name}</h1>
-                                    <h2 className="text-xl font-light text-slate-50 mb-3">{description}</h2>
-                                    <p className="border-t-2 border-slate-50"></p>
-                                    <span className="inline-block rounded-full bg-slate-50 font-semibold text-slate-600 px-3 py-2 mt-3">{language}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-            </ul>
-        </li>
+            <div className="mt-7">
+                <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">{name}</h3>
+                <p className="mt-3 text-gray-800 dark:text-neutral-200">{description}</p>
+                <p className="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600 decoration-2 group-hover:underline">Read more
+                    <svg className="size-4 shrink-0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
+                </p>
+            </div>
+        </a>
     )
 }
--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -26,14 +26,10 @@ export default function Product() {
                 viewport={{ once: true, amount: 0 }}
             >
                 <h1 className="pb-3 text-center text-5xl font-semibold text-slate-900">Products</h1>
-                <p className="text-center text-lg text-slate-900">今までに作ったもの</p>
-                <div>
-                    <div className="mx-auto max-w-6xl px-6 lg:px-8">
-                        <ul className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-0 gap-y-0 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                            <ProductCard name="zawataku CSS" description="自作CSSフレームワーク" language="CSS" imgURL="/img/products/zawatakuCSS.webp" linkURL="https://github.com/zawataku/zawataku_framework"></ProductCard>
-                        </ul >
-                    </div >
-                </div >
+                <p className=" mb-7 text-center text-lg text-slate-900">今までに作ったものたち</p>
+                <div className="mx-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    <ProductCard name="zawataku CSS Framework" description="自作汎用CSSフレームワークです．大量のCSSコードを書くことなく美しいデザインのWebページを作ることができます．" imgURL="/img/products/zawatakuCSS.webp" linkURL="https://github.com/zawataku/zawataku_framework"></ProductCard>
+                </div>
             </motion.div>
         </div >
     )
