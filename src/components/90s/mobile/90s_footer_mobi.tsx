751b49472c89e704ad09203b4bd30ca65675caa7
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-06-11 13:07:40 +0900

    リダイレクト作業

--- src/components/90s/mobile/90s_footer_mobi.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Footer_90s() {
-    return (
-        <div className="mt-10 block h-20 w-full bg-red-400 md:hidden lg:hidden">
-            <p className="pt-2 text-center text-white">ざわたくの部屋　-Since 2023-</p>
-            <p className="mx-auto mt-2 text-center text-white">Copyright © 2024 ざわたく All rights reserved.</p>
-        </div >
-    );
-}
--- src/components/90s/mobile/90s_top_mobi.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Top_Mobi_90s (){
-    return(
-        <div className="m-10">
-            <img src="/img/90s/90s_top_img.webp" alt="トップ画像" />
-        </div>
-    )
-}
